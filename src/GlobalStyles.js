add72be117a3207b59ea0f1249fc4b3a2c7df231
kennyd <kennyd@btcex.ch>
2018-06-12 09:27:30 +0800

    first commit

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    welcome: {
+        fontSize: 20
+    }
+});
+
+/*
+
+how to use
+import styles from './GlobalStyles'
+
+<View style={styles.container}>{this.props.children}</View>
+ */
